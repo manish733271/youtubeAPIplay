962149e58221205d5499654f303ce80f5a23449f
Manish Adhikari <nepali.adhikari@gmail.com>
2017-01-08 23:26:48 +0100

    Youtube api successfully implemented

--- src/index.jsx
+++ src/index.jsx
@@ -3,20 +3,29 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyC7U7508YZgVgYtXs74FgZO65vQCLY_xqQ';
 
-
-
 // create a new component
 class App extends Component {
+
   constructor(props){
     super(props);
 
-    this.state = { videos:[] };
+    this.state = { videos:[],
+    selectedVideo: null
+    };
 
-    YTSearch({key:API_KEY, term:'surfboards'}, (videos) => {
-      this.setState({ videos });
+    this.videoSearch('surf');
+  }
+
+  videoSearch(term){
+    YTSearch({key:API_KEY, term:term}, (videos) => {
+      this.setState({
+        videos,
+        selectedVideo: videos[0]
+       });
       //this.setState({videos: vidoes})
     });
   }
@@ -24,8 +33,11 @@ class App extends Component {
   render(){
     return(
       <div>
-        <SearchBar />
-        <VideoList videos = { this.state.videos} />
+        <SearchBar onSearch = {term => this.videoSearch(term) } />
+        <VideoDetail video = { this.state.selectedVideo } />
+        <VideoList
+          onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
+          videos = { this.state.videos} />
       </div>
     );
   }
@@ -34,13 +46,3 @@ class App extends Component {
 
 //Take this component and put it in html
 ReactDOM.render(<App />, document.querySelector('.container'));
-
-
-
-// const App = () => {
-//   return(
-//     <div>
-//       <SearchBar />
-//     </div>
-//   );
-// }
