8459dd82599459e16c6c687e3673d25159b047c8
Manish Adhikari <nepali.adhikari@gmail.com>
2017-01-08 19:15:53 +0100

    some made in search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,5 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-  return <input />;
+class SearchBar extends Component {
+  constructor(props){
+    super(props);
+    this.state = {term: ''};
+  }
+
+  render(){
+    return(
+      <div>
+       <input
+        value = { this.state.term }
+        onChange = {event => this.setState({term: event.target.value})} />
+      //  Value of the state: { this.state.term }
+      </div>
+     );
+  }
 }
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyC7U7508YZgVgYtXs74FgZO65vQCLY_xqQ';
 
 // create a new component
 const App = () => {
-  return <div>Hi! This is Manish</div>;
+  return(
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 //Take this component and put it in html
